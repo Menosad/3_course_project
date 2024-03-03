60eef91a3e7f2e5d0af5d16e7c40935fefc564ee
menosad <draignair@gmail.com>
2024-03-03 17:05:27 +0300

    added function getting_list_operation

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,34 @@
-import os
 import pprint
-import json
-
-#Распаковываем и переводим в список словарей файл с банковскими операциями
-with open('../operations.json', 'r') as file:
-    operations_list = json.load(file)
+from clases import Operation
 
 
+
+def getting_list_operation(array: list):
+    list_ = []
+    for dic in array:
+        if 'date' not in dic.keys():
+            pass
+        elif 'from' not in dic.keys():
+            date = dic['date']
+            unick = dic['id']
+            state = dic['state']
+            amount = dic['operationAmount']
+            descrpt = dic['description']
+            agent = None
+            accaunt = dic['to']
+            oper_n = Operation(date, unick, state,
+                               amount, descrpt, agent, accaunt)
+            list_.append(oper_n)
+        else:
+            date = dic['date']
+            unick = dic['id']
+            state = dic['state']
+            amount = dic['operationAmount']
+            descrpt = dic['description']
+            agent = dic['from']
+            accaunt = dic['to']
+            oper_n = Operation(date, unick, state,
+                               amount, descrpt, agent, accaunt)
+            list_.append(oper_n)
+
+    return list_

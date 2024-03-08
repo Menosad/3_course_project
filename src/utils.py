d7c5fa7412c4bb83fcd6db85a5c0cb2f26a8a2ba
menosad <draignair@gmail.com>
2024-03-08 09:12:27 +0300

    added new functions: interval_selection conclusion

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,10 @@ import pprint
 from clases import Operation
 
 
-
 def getting_list_operation(array: list):
     list_ = []
     for dic in array:
-        if 'date' not in dic.keys():
+        if 'date' not in dic:
             pass
         elif 'from' not in dic.keys():
             date = dic['date']
@@ -16,8 +15,7 @@ def getting_list_operation(array: list):
             descrpt = dic['description']
             agent = None
             accaunt = dic['to']
-            oper_n = Operation(date, unick, state,
-                               amount, descrpt, agent, accaunt)
+            oper_n = Operation(date, descrpt, agent, unick, amount, state, accaunt)
             list_.append(oper_n)
         else:
             date = dic['date']
@@ -27,9 +25,27 @@ def getting_list_operation(array: list):
             descrpt = dic['description']
             agent = dic['from']
             accaunt = dic['to']
-            oper_n = Operation(date, unick, state,
-                               amount, descrpt, agent, accaunt)
+            oper_n = Operation(date, descrpt, agent, unick, amount, state, accaunt)
             list_.append(oper_n)
 
     return list_
 
+
+def interval_selection(lst: list):
+    interval = []
+    for object in lst:
+        if object.state == 'EXECUTED':
+            interval.append(object)
+    return interval
+
+
+def conclusion(lst: list):
+    for operation in lst[:10]:
+        if operation.agent == None:
+            print(f"{operation.f_date()} {operation.descrpt}\n"
+                  f"{operation.check()}\n"
+                  f"{operation.money()}")
+        else:
+            print(f"{operation.f_date()} {operation.descrpt}\n"
+                  f"{operation.f_agent()} -> {operation.check()}\n"
+                  f"{operation.money()}")

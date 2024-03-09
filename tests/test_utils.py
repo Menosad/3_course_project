d90448d8e82b1e039f355fa33473943d2fa3cace
menosad <draignair@gmail.com>
2024-03-10 01:04:19 +0300

    added tests, coverage more then 80%

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,31 @@
-import pytest
-
 from src import utils
 import os
 
 coll = [
-    {"date": "2019-08-26T10:50:58.294041", "state": "EXECUTED"},
+    {"date": "2019-08-26T10:50:58.294041", "state": "EXECUTED", },
     {"date": "2019-07-03T18:35:29.512364", "state": "CANCELED"},
     {}
 ]
 
-col = {"date": "2019-08-26T10:50:58.294041", "state": "EXECUTED"}
+col_date = {"date": "2019-08-26T10:50:58.294041",
+            "description": "Перевод организации"}
+
+col_second_line = {"from": "Maestro 1596837868705199",
+                   "to": "Счет 64686473678894779589"}
+
+col_second_line1 = {"from": "Счет 27248529432547658655",
+                    "to": "Счет 97584898735659638967"}
+col_second_line2 = {"from": "Visa Platinum 2256483756542539",
+                    "to": "Счет 78808375133947439319"}
+col_second_line3 = {"from": "Счет 17066032701791012883",
+                    "to": "Visa Classic 4195191172583802"}
+col_second_line4 = {"to": "Visa Classic 4195191172583802"}
 
 PATH = os.path.join('test_json_file.json')
+
+amaunt_col = {"operationAmount": {"amount": "49192.52", "currency": {
+    "name": "USD",
+    "code": "USD"}}}
 
 
 def test_get_file():
@@ -26,5 +40,16 @@ def test_get_operation_list():
     assert utils.get_operation_list(coll) == [{"date": "2019-08-26T10:50:58.294041", "state": "EXECUTED", }]
 
 
-def test_get_date():
-    assert utils.get_date(col) == '26.08.2019'
+def test_get_first_line():
+    assert utils.get_first_line(col_date) == '26.08.2019 Перевод организации'
+
+
+def test_get_second_line():
+    assert utils.get_second_line(col_second_line) == 'Maestro 1596 83** **** 5199 -> Счет **9589'
+    assert utils.get_second_line(col_second_line1) == 'Счет **8655 -> Счет **8967'
+    assert utils.get_second_line(col_second_line2) == 'Visa Platinum 2256 48** **** 2539 -> Счет **9319'
+    assert utils.get_second_line(col_second_line3) == 'Счет **2883 -> Visa Classic 4195 19** **** 3802'
+    assert utils.get_second_line(col_second_line4) == '-> Visa Classic 4195 19** **** 3802'
+
+def test_third_line():
+    assert utils.get_third_line(amaunt_col) == '49192.52 USD.'

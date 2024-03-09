f29e229fdc3eb9524182d216a6ddd12b8eb468ad
menosad <draignair@gmail.com>
2024-03-09 22:22:24 +0300

    added function date

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,19 @@
 import json
-import os
 
-
-
-PATH = os.path.join('..', 'operations.json')
-
-
-def get_operation_list():
-    with open(PATH, encoding='utf-8') as file:
+def get_file(pth):
+    with open(pth, encoding='utf-8') as file:
         lst = json.load(file)
-        filt = list(filter(lambda x: x.get('date'), lst))
-        filtred_list = list(filter(lambda s: s['state'] == 'EXECUTED', filt))
-        final = sorted(filtred_list, key=lambda d: d['date'], reverse=True)
-    return final
+    return lst
 
 
+def get_operation_list(dic: list):
+    dic_filtred = list(filter(lambda x: x.get('date'), dic))
+    filtred_list = list(filter(lambda e: e['state'] == 'EXECUTED', dic_filtred))
+    lst = sorted(filtred_list, key=lambda d: d['date'], reverse=True)
+    return lst
 
+def get_date(col: dict):
+    s = col['date']
+    ind = s.find('T')
+    lst = s[:ind].split('-')
+    return lst[-1] + '.' + lst[-2] + '.' + lst[-3]

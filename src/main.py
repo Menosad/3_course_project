71289eecd74bdfbd7e5736bb105fbe1118b91ffc
Menosad <95692010+Menosad@users.noreply.github.com>
2024-03-10 07:16:35 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from os import path
 import utils
 
+#Получаем путь к файлу operations.json
 PATH = path.join('..', 'operations.json')
+#Формируем исходный список операций
 lst = utils.get_file(PATH)
+#Получаем требуемый список операций, фильруем, сортируем по дате
 operation_list = utils.get_operation_list(lst)
 
+#Вывод требуемой информации из списка операций
 for o in operation_list[:5]:
     print(utils.get_first_line(o))
     print(utils.get_second_line(o))
